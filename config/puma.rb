fcda0886d14b8ded38e6b6c7daf5b40d18734e67
teqshark <teqpros@gmail.com>
2022-06-03 23:22:11 -0700

    new config/puma.rb file

--- config/puma.rb
+++ config/puma.rb
@@ -10,9 +10,9 @@
 
 
 
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+# max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+# min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+# threads min_threads_count, max_threads_count
 
 
 
@@ -23,10 +23,17 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-const PORT = process.env.PORT || 6565;
-console.log('logging PORT VAR');
-console.log(PORT);
-app.listen(PORT );
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+
 # app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
 #     console.log("Server is running.");
 #   });
@@ -36,7 +43,7 @@ app.listen(PORT );
 #   });
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "production" }
+# environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
@@ -49,22 +56,22 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 # example config from heroku
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-preload_app!
+# preload_app!
 
 
 
-  on_worker_boot do
+#   on_worker_boot do
     # Worker specific setup for Rails 4.1+
     # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-    ActiveRecord::Base.establish_connection
-  end
+    # ActiveRecord::Base.establish_connection
+#   end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
